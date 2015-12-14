5b0b9e37d3dfa5db9696bf58a853a9b5ece89a2b
Anatoly Chernow <f0ck1ng.namespace@gmail.com>
2015-12-14 08:59:05 +0200

    No obvious benefits in autoloading here

--- lib/hobby.rb
+++ lib/hobby.rb
@@ -4,8 +4,8 @@ require 'forwardable'
 
 module Hobby
   Verbs = %w!DELETE GET HEAD OPTIONS PATCH POST PUT!
-  require 'hobby/app'
-  autoload :Router, 'hobby/router'
-  autoload :Middleware, 'hobby/middleware'
   include_constants :Builder, :Request, :Response, from: Rack
 end
+
+require 'hobby/router'
+require 'hobby/app'
