a507f8b7716f7f33de9af342cd3770dfa61e9fad
Anatoly Chernow <f0ck1ng.namespace@gmail.com>
2016-10-27 02:45:23 +0300

    Eliminate three redundant tests of Hobby::Router

--- spec/helper.rb
+++ spec/helper.rb
@@ -7,6 +7,14 @@ require 'minitest'
 require 'minitest-power_assert'
 Minitest::Assertions.prepend Minitest::PowerAssert::Assertions
 
+if defined? Mutant
+  class Mutant::Selector::Expression
+    def call _subject
+      integration.all_tests
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec, :minitest
   config.include Hobby::RSpec::Router, type: :router
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -29,32 +29,9 @@ describe Hobby::Router, type: :router do
   end
 
 
-  # Below is a bunch of implementation-dependent tests which should not really be there
-  # TODO: find a way to get rid of them
-
-  it 'uses a verb to imply the root route' do
-    subject.add_route 'GET', '/' do :route end
-
-    assert { not subject.instance_variable_get(:@routes).include? 'GET//' }
-  end
-
   it 'memoizes requests with params' do
     subject.route_for env_for '/hello/ololo'
 
     assert { subject.instance_variable_get(:@routes).include? 'GET/hello/ololo' }
-  end
-
-  it 'does not touch path params when there is nothing to set'do
-    env = env_for ''
-    subject.route_for env
-    
-    assert { env.fetch(:path_params, {}).is_a? Hash }
-  end
-
-  it 'sets path params when needed' do
-    env = env_for '/hello/ololo'
-    subject.route_for env
-
-    assert { env[:path_params].is_a? Hash }
   end
 end
