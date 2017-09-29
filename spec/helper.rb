c0b5966a93aa9b4cf80ee5b66c128656137711ba
Anatoly Chernow <chertoly@gmail.com>
2017-09-29 14:51:42 +0300

    Extract spec/mutant_patches.rb

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,37 +1,11 @@
 require 'devtools/spec_helper'
 
 require 'hobby'
+require_relative 'mutant_patches' if defined? Mutant
 
 require 'minitest'
 require 'minitest-power_assert'
 Minitest::Assertions.prepend Minitest::PowerAssert::Assertions
-
-if defined? Mutant
-  class Mutant::Selector::Expression
-    def call _subject
-      integration.all_tests
-    end
-  end
-
-  class Mutant::Isolation::Fork
-    def result
-      yield
-    end
-  end
-
-  class Mutant::Loader
-    def call
-      source = Unparser.unparse node
-
-      puts <<~S
-        Current mutantion:
-        #{source}
-      S
-
-      kernel.eval source, binding, subject.source_path.to_s, subject.source_line
-    end
-  end
-end
 
 module EnvFor
   def env_for path, verb = 'GET'
